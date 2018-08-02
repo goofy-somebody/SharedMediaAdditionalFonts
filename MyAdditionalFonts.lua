96309f1bdb6222b09c4be7458239f1f959eb858d
forumuser599 <forumuser599@outlook.com>
2018-08-02 14:51:24 -0500

    first commit

---
+++ MyAdditionalFonts.lua
@@ -0,0 +1,33 @@
+--[[
+
+----
+---- PLEASE READ BEFORE DOING ANYTHING ----
+----
+
+Only fonts should be added to this addon. If you wish to add textures, sounds, ... create your own addon or use an existing one if there is one.
+All fonts must be able to display special characters such as é, è, à, ü, É, Ç, ... Remember that not all the users use the English client!
+
+Don't add a font that you did not test. All fonts must be tested in game before upload.
+
+Thanks for your cooperation! ;-)
+pb_ee1
+
+]]
+
+-- registrations for media from the client itself belongs in LibSharedMedia-3.0
+
+if not SharedMediaAdditionalFonts then return end
+
+-- -----
+-- FONT
+-- -----
+SharedMediaAdditionalFonts:Register("font", "Aovel Sans Black", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\ASansBlack.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Ace Futurism", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\ace_futurism.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Oregon LDO Black", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\Oregon_LDO_Black.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Oregon LDO Bold", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\Oregon_LDO_Bold.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Playball", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\Playball.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Special Elite", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\SpecialElite.ttf]])
+SharedMediaAdditionalFonts:Register("font", "SpicyRice", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\SpicyRice_Regular.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Star Jedi", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\Star_Jedi_Rounded.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Swansea", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\SWANSE__.ttf]])
+SharedMediaAdditionalFonts:Register("font", "Swansea Bold", [[Interface\Addons\SharedMediaAdditionalFonts\fonts\SWANSE_B.ttf]])
